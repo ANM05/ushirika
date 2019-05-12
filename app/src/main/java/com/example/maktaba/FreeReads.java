e8aa9e0fab2001211b1054b88d47bb9e0240f7c5
Antony Nganga <antonynganga05@gmail.com>
2019-05-12 23:00:15 +0300

    Add Arrays for free reads activity

--- app/src/main/java/com/example/maktaba/FreeReads.java
+++ app/src/main/java/com/example/maktaba/FreeReads.java
@@ -2,8 +2,14 @@ package com.example.maktaba;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class FreeReads extends AppCompatActivity {
+    private ListView mList;
+
+    int[] mImages = {R.drawable.art1, R.drawable.art2, R.drawable.art3, R.drawable.art4};
+    String[] mTitle = {"Book One", "Book Two", "Book Three", "Book Four"};
+    String[] mDescription ={"Book one description", "Book two description", "Book three description", "Book four description"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
