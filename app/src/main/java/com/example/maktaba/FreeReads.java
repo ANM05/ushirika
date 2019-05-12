38d9bb7d18a96154470c1539c65b300d0b9b4e41
Antony Nganga <antonynganga05@gmail.com>
2019-05-12 23:39:12 +0300

    Refactor category custom adapter

--- app/src/main/java/com/example/maktaba/FreeReads.java
+++ app/src/main/java/com/example/maktaba/FreeReads.java
@@ -5,10 +5,13 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class FreeReads extends AppCompatActivity {
     private ListView mList;
@@ -23,6 +26,8 @@ public class FreeReads extends AppCompatActivity {
         setContentView(R.layout.activity_free_reads);
 
         mList.findViewById(R.id.categoryListId);
+        CategoryAdapter adapter = new CategoryAdapter(this, mImages, mTitle, mDescription);
+        mList.setAdapter(adapter);
     }
 
     public class CategoryAdapter extends ArrayAdapter<String>{
@@ -42,7 +47,18 @@ public class FreeReads extends AppCompatActivity {
         @NonNull
         @Override
         public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-            return super.getView(position, convertView, parent);
+            LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View custom_categories = layoutInflater.inflate(R.layout.custom_categories, parent, false);
+
+            ImageView images= custom_categories.findViewById(R.id.cat_image);
+            TextView title = custom_categories.findViewById(R.id.cat_txt);
+            TextView description = custom_categories.findViewById(R.id.cat_desc);
+
+            images.setImageResource(rImages[position]);
+            title.setText(rTitle[position]);
+            description.setText(rDescription[position]);
+
+            return custom_categories;
         }
     }
 }
