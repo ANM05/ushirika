8ff18a3c111d4150d0ba8c5c3273abac5e49fc80
Antony Nganga <antonynganga05@gmail.com>
2019-05-12 16:56:52 +0300

    Add butterKnife to dependencies

--- app/src/main/java/com/example/maktaba/categories.java
+++ app/src/main/java/com/example/maktaba/categories.java
@@ -2,8 +2,12 @@ package com.example.maktaba;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class categories extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
