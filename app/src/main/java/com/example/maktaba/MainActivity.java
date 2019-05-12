a8f8e8231636bee9ac7be1d53819b41368237792
Antony Nganga <antonynganga05@gmail.com>
2019-05-12 15:36:14 +0300

    Add custom adapter for mainactivity

--- app/src/main/java/com/example/maktaba/MainActivity.java
+++ app/src/main/java/com/example/maktaba/MainActivity.java
@@ -1,13 +1,73 @@
 package com.example.maktaba;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mList;
+
+    int[] mImages = {R.drawable.trev,
+                    R.drawable.wisdom,
+                    R.drawable.ngugi,
+                    R.drawable.sample_book};
+
+    String[] mBooks= {"Born A Crime","Wisdom of Insecurity", "Decolonizing the Mind", "My Adventures As An Illustrator"};
+    String[] mDescription = {"Born a crime description", "Wisdom of insecurity description", "Decolonizing the mind description", "My adventures as an illustrator description"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mList = findViewById(R.id.picks_list);
+        MainAdapter mainAdapter = new MainAdapter(this, mBooks, mDescription, mImages);
+        mList.setAdapter(mainAdapter);
+    }
+
+    class MainAdapter extends ArrayAdapter<String> {
+
+        Context context;
+        String[] rBooks;
+        String[] rDescription;
+        int[] rImages;
+
+        MainAdapter (Context c, String[] books, String[] description, int[] images){
+            super(c, R.layout.main_custom, R.id.img_txt, books);
+            this.context= c;
+            this.rBooks = books;
+            this.rDescription= description;
+            this.rImages= images;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View main_custom = layoutInflater.inflate(R.layout.main_custom, parent, false);
+
+            ImageView imageView = main_custom.findViewById(R.id.sample_image);
+            TextView books = main_custom.findViewById(R.id.img_txt);
+            TextView desc = main_custom.findViewById(R.id.img_desc);
+
+            imageView.setImageResource(rImages[position]);
+            books.setText(rBooks[position]);
+            desc.setText(rDescription[position]);
+
+            return main_custom;
+        }
     }
 }
+
+
