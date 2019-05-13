d4ea0d9ba04b8dea7c635a84c6c11530ae987fe2
Antony Nganga <antonynganga05@gmail.com>
2019-05-13 22:58:31 +0300

    Create intent for login button

--- app/src/main/java/com/example/maktaba/login.java
+++ app/src/main/java/com/example/maktaba/login.java
@@ -1,13 +1,40 @@
 package com.example.maktaba;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class login extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class login extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.btn_login)
+    Button mLogin;
+    @BindView(R.id.username)
+    EditText mUsername;
+    @BindView(R.id.password) EditText mPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        mLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v == mLogin){
+            String userName = mUsername.getText().toString();
+            String password = mPassword.getText().toString();
+            Intent intent = new Intent(login.this, MainActivity.class);
+            intent.putExtra("username", userName);
+            intent.putExtra("password", password);
+            startActivity(intent);
+        }
     }
 }
